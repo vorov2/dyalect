7d1dc8c2712bd8be07472eb7114c6f4e2fcc8d4a
vorov2 <basil@voronkov.name>
2019-04-16 12:42:51 +0300

    Debugger enhancements

---
+++ Dyalect/Debug/StackPoint.cs
@@ -0,0 +1,15 @@
+﻿namespace Dyalect.Debug
+{
+    public struct StackPoint
+    {
+        internal StackPoint(int breakAddress, int moduleHandle)
+        {
+            BreakAddress = breakAddress;
+            ModuleHandle = moduleHandle;
+        }
+
+        internal readonly int BreakAddress;
+
+        internal readonly int ModuleHandle;
+    }
+}
