47b73d818fb31c1425f5df48158193badf8bdbf6
Василий Воронков <basil@voronkov.name>
2019-04-24 21:53:51 +0300

    Changed testing framework

--- Dyalect.UnitTests/Tests.cs
+++ Dyalect.UnitTests/Tests.cs
@@ -5,7 +5,7 @@ using Dyalect.Parser;
 using Dyalect.Parser.Model;
 using Dyalect.Runtime;
 using Dyalect.Runtime.Types;
-using NUnit.Framework;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -14,77 +14,78 @@ using System.Runtime.CompilerServices;
 
 namespace Tests
 {
+    [TestClass]
     public class Tests
     {
-        [Test] public void MathTest1() => ShouldBe(38);
+        [TestMethod] public void MathTest1() => ShouldBe(38);
 
-        [Test] public void MathTest2() => ShouldBe(1.5919117647058822);
+        [TestMethod] public void MathTest2() => ShouldBe(1.5919117647058822);
 
-        [Test] public void MathTest3() => ShouldBe(6.6129032258064528);
+        [TestMethod] public void MathTest3() => ShouldBe(6.6129032258064528);
 
-        [Test] public void MathTest4() => ShouldBe(11.256);
+        [TestMethod] public void MathTest4() => ShouldBe(11.256);
 
-        [Test] public void MathTest5() => ShouldBe(0.0099999999999997868);
+        [TestMethod] public void MathTest5() => ShouldBe(0.0099999999999997868);
 
-        [Test] public void MathTest6() => ShouldBe(783.59999999999991);
+        [TestMethod] public void MathTest6() => ShouldBe(783.59999999999991);
 
-        [Test] public void LogicalTest1() => ShouldBe(true);
+        [TestMethod] public void LogicalTest1() => ShouldBe(true);
 
-        [Test] public void LogicalTest2() => ShouldBe(false);
+        [TestMethod] public void LogicalTest2() => ShouldBe(false);
 
-        [Test] public void ComparisonTest1() => ShouldBe(true);
+        [TestMethod] public void ComparisonTest1() => ShouldBe(true);
 
-        [Test] public void ComparisonTest2() => ShouldBe(true);
+        [TestMethod] public void ComparisonTest2() => ShouldBe(true);
 
-        [Test] public void ToStringTest1() => ShouldBe("(1, 2, 3)");
+        [TestMethod] public void ToStringTest1() => ShouldBe("(1, 2, 3)");
 
-        [Test] public void ToStringTest2() => ShouldBe("[12.2, \"string\", true]");
+        [TestMethod] public void ToStringTest2() => ShouldBe("[12.2, \"string\", true]");
 
-        [Test] public void ToStringTest3() => ShouldBe("(x: 42, y: \"foo\")");
+        [TestMethod] public void ToStringTest3() => ShouldBe("(x: 42, y: \"foo\")");
 
-        [Test] public void WhileTest1() => ShouldBe(33);
+        [TestMethod] public void WhileTest1() => ShouldBe(33);
 
-        [Test] public void WhileTest2() => ShouldBe(42);
+        [TestMethod] public void WhileTest2() => ShouldBe(42);
 
-        [Test] public void FizzbuzzTest() => ShouldBe("12fizz4buzzfizz78fizzbuzz");
+        [TestMethod] public void FizzbuzzTest() => ShouldBe("12fizz4buzzfizz78fizzbuzz");
 
-        [Test] public void RecursionTest() => ShouldBe(10);
+        [TestMethod] public void RecursionTest() => ShouldBe(10);
 
-        [Test] public void FactTest() => ShouldBe(2432902008176640000);
+        [TestMethod] public void FactTest() => ShouldBe(2432902008176640000);
 
-        [Test] public void PowerTest() => ShouldBe(1000);
+        [TestMethod] public void PowerTest() => ShouldBe(1000);
 
-        [Test] public void BinaryConversionTest() => ShouldBe("10011101");
+        [TestMethod] public void BinaryConversionTest() => ShouldBe("10011101");
 
-        [Test] public void FibTest1() => ShouldBe(89);
+        [TestMethod] public void FibTest1() => ShouldBe(89);
 
-        [Test] public void FibTest2() => ShouldBe("0 1 1 2 3 5 8 13 21 34 55");
+        [TestMethod] public void FibTest2() => ShouldBe("0 1 1 2 3 5 8 13 21 34 55");
 
-        [Test] public void PhoneParserTest() => ShouldBe("9645061112");
+        [TestMethod] public void PhoneParserTest() => ShouldBe("9645061112");
 
-        [Test] public void IteratorTest1() => ShouldBe(116.8);
+        [TestMethod] public void IteratorTest1() => ShouldBe(116.8);
 
-        [Test] public void IteratorTest2() => ShouldBe(36);
+        [TestMethod] public void IteratorTest2() => ShouldBe(36);
 
-        [Test] public void IteratorTest3() => ShouldBe(20);
+        [TestMethod] public void IteratorTest3() => ShouldBe(20);
 
-        [Test] public void IteratorTest4() => ShouldBe("olleH");
+        [TestMethod] public void IteratorTest4() => ShouldBe("olleH");
 
-        [Test] public void IteratorTest5() => ShouldBe("ll");
+        [TestMethod] public void IteratorTest5() => ShouldBe("ll");
 
-        [Test] public void IteratorTest6() => ShouldBe(8);
+        [TestMethod] public void IteratorTest6() => ShouldBe(8);
 
-        [Test] public void CalcETest() => ShouldBe(2.7182818284590455);
+        [TestMethod] public void CalcETest() => ShouldBe(2.7182818284590455);
 
-        [Test] public void DammTest() => ShouldBe("yesnoyesno");
+        [TestMethod] public void DammTest() => ShouldBe("yesnoyesno");
 
-        [Test] public void GcdTest() => ShouldBe(12);
+        [TestMethod] public void GcdTest() => ShouldBe(12);
 
-        [Test] public void EulerTest() => ShouldBe("0=100;10=44;20=27.2;30=22.16;40=20.648;50=20.1944;60=20.05832;70=20.017496;80=20.0052488;90=20.00157464;100=20.000472392;");
+        [TestMethod] public void EulerTest() => ShouldBe("0=100;10=44;20=27.2;30=22.16;40=20.648;50=20.1944;60=20.05832;70=20.017496;80=20.0052488;90=20.00157464;100=20.000472392;");
 
-        [Test] public void AckTest() => ShouldBe(125);
+        [TestMethod] public void AckTest() => ShouldBe(125);
 
-        [Test] public void FoldTest() => ShouldBe(21);
+        [TestMethod] public void FoldTest() => ShouldBe(21);
 
         #region Execution
         private void ShouldBe(int expected, [CallerMemberName]string callerName = "") =>
@@ -93,7 +94,7 @@ namespace Tests
         private void ShouldBe(object expected, [CallerMemberName]string callerName = "")
         {
             var res = RunTest(callerName).ToObject();
-            Assert.True(expected.Equals(res), $"Expected <<{expected}>>, got <<{res}>>.");
+            Assert.IsTrue(expected.Equals(res), $"Expected <<{expected}>>, got <<{res}>>.");
         }
 
         private Dictionary<string, DyCodeModel> ast;
