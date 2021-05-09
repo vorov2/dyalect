d23fff42e6f01d3598e94844d43ccce8a12a497c
vorov2 <basil@voronkov.name>
2021-05-09 23:47:05 +0300

    -

--- Dyalect/Runtime/CallStack.cs
+++ Dyalect/Runtime/CallStack.cs
@@ -80,7 +80,7 @@ namespace Dyalect.Runtime
         public readonly int Offset;
         public readonly DyNativeFunction Function;
 
-        public Caller()
+        internal Caller()
         {
             Locals = Array.Empty<DyObject>();
             EvalStack = new EvalStack(0);
