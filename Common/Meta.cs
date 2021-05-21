e91ad67f42ea13ef3397223e3d4d7e954b818241
Василий Воронков <basil@voronkov.name>
2021-05-21 21:09:45 +0300

    -

--- Common/Meta.cs
+++ Common/Meta.cs
@@ -1,16 +1,19 @@
-public static class Meta
+namespace Dyalect
 {
-    public static readonly System.DateTime Epoch = new(2019, 4, 17);
+    public static class Meta
+    {
+        public static readonly System.DateTime Epoch = new(2019, 4, 17);
 
-    public const string Version = "0.26.0";
+        public const string Version = "0.26.0";
 
-    public const string Product = "Dyalect";
+        public const string Product = "Dyalect";
 
-    public const string Company = "Vasily Voronkov";
+        public const string Company = "Vasily Voronkov";
 
-    public const string Description = "Dyalect, a dynamic language for .NET Core.";
+        public const string Description = "Dyalect, a dynamic language for .NET Core.";
 
-    public const string Copyright = "Copyright (c) Vasily Voronkov 2019-2021";
+        public const string Copyright = "Copyright (c) Vasily Voronkov 2019-2021";
 
-    public const string Trademark = "Vasily Voronkov";
+        public const string Trademark = "Vasily Voronkov";
+    }
 }
