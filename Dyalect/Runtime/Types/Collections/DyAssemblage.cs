e0f0549f3c36b27df2e5ec4b47bc274891972ced
vorov2 <basil@voronkov.name>
2021-05-29 01:14:17 +0300

    -

---
+++ Dyalect/Runtime/Types/Collections/DyAssemblage.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Dyalect.Runtime.Types
+{
+    internal sealed class DyAssemblage : DyObject
+    {
+        internal readonly DyObject[] Values;
+        private readonly string[] keys;
+        private readonly int[] readOnly;
+
+        public int Count => Values.Length;
+        
+        public DyAssemblage(DyObject[] locals, string[] keys, int[] readOnly) : base(DyType.Object) =>
+            (Values, this.keys, this.readOnly) = (locals, keys, readOnly);
+
+        public int GetOrdinal(string name)
+        {
+            for (var i = 0; i < Values.Length; i++)
+                if (Values[i].GetLabel() == name)
+                    return i;
+
+            return -1;
+        }
+
+        public override object ToObject() => throw new NotSupportedException();
+
+        public override int GetHashCode() => throw new NotSupportedException();
+    }
+}
