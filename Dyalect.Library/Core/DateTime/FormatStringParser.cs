5a467c99f3386745226feb87c3b6638999373f75
vorov2 <basil@voronkov.name>
2022-04-20 17:22:32 +0300

    -

--- Dyalect.Library/Core/DateTime/FormatStringParser.cs
+++ Dyalect.Library/Core/DateTime/FormatStringParser.cs
@@ -6,169 +6,47 @@ namespace Dyalect.Library.Core
     internal enum FormatElementKind
     {
         Literal,
-        d,
-        dd,
-        ddd,
-        dddd,
-        h,
-        hh,
-        H,
-        HH,
-        m,
-        mm,
-        M,
-        MM,
-        MMM,
-        MMMM,
-        s,
-        ss,
-        t,
-        tt,
-        f,
-        ff,
-        fff,
-        ffff,
-        fffff,
-        ffffff,
-        fffffff,
-        F,
-        FF,
-        FFF,
-        FFFF,
-        FFFFF,
-        FFFFFF,
-        FFFFFFF,
-        y,
-        yy,
-        yyy,
-        yyyy,
-        yyyyy,
-        g,
-        K,
-        z,
-        zz,
-        zzz
+        Plus,
+        Day,
+        Hour,
+        Minute,
+        Second,
+        Decisecond,
+        Centisecond,
+        Millisecond,
+        Microsecond,
+        Tick
     }
 
-    internal record FormatElement(FormatElementKind Kind, string Value);
+    internal record FormatElement(FormatElementKind Kind, string Value, int Padding = 0, bool DisplayIfZero = true);
 
     internal sealed class FormatStringParser
     {
         private readonly FormatElement[] elements;
-        private static readonly FormatElement[] timeDeltaElements = new FormatElement[] 
+        private static readonly FormatElement[] timeDeltaElements = new FormatElement[]
         {
-            new(d, "d"),
-            new(dd, "dd"),
-            new(h, "h"),
-            new(hh, "hh"),
-            new(m, "m"),
-            new(mm, "mm"),
-            new(s, "s"),
-            new(ss, "ss"),
-            new(f, "f"),
-            new(ff, "ff"),
-            new(fff, "fff"),
-            new(ffff, "ffff"),
-            new(fffff, "fffff"),
-            new(ffffff, "ffffff"),
-            new(fffffff, "fffffff"),
-            new(F, "F"),
-            new(FF, "FF"),
-            new(FFF, "FFF"),
-            new(FFFF, "FFFF"),
-            new(FFFFF, "FFFFF"),
-            new(FFFFFF, "FFFFFF"),
-            new(FFFFFFF, "FFFFFFF"),
+            new(Plus, "+"),
+            new(Day, "dd", 2),
+            new(Day, "d"),
+            new(Hour, "hh", 2),
+            new(Hour, "h"),
+            new(Minute, "mm", 2),
+            new(Minute, "m"),
+            new(Second, "ss", 2),
+            new(Second, "s"),
+            new(Tick, "fffff", 7),
+            new(Microsecond, "ffff", 6),
+            new(Millisecond, "fff", 3),
+            new(Centisecond, "ff", 2),
+            new(Decisecond, "f"),
         };
         private static readonly FormatElement[] dateTimeElements = new FormatElement[]
         {
-            new(d, "d"),
-            new(dd, "dd"),
-            new(ddd, "ddd"),
-            new(dddd, "dddd"),
-            new(M, "M"),
-            new(MM, "MM"),
-            new(MMM, "MMM"),
-            new(MMMM, "MMMM"),
-            new(m, "m"),
-            new(mm, "mm"),
-            new(h, "h"),
-            new(hh, "hh"),
-            new(H, "H"),
-            new(HH, "HH"),
-            new(g, "g"),
-            new(f, "f"),
-            new(ff, "ff"),
-            new(fff, "fff"),
-            new(ffff, "ffff"),
-            new(fffff, "fffff"),
-            new(ffffff, "ffffff"),
-            new(fffffff, "fffffff"),
-            new(F, "F"),
-            new(FF, "FF"),
-            new(FFF, "FFF"),
-            new(FFFF, "FFFF"),
-            new(FFFFF, "FFFFF"),
-            new(FFFFFF, "FFFFFF"),
-            new(FFFFFFF, "FFFFFFF"),
-            new(s, "s"),
-            new(ss, "ss"),
-            new(t, "t"),
-            new(tt, "tt"),
-            new(t, "t"),
-            new(tt, "tt"),
-            new(y, "y"),
-            new(yy, "yy"),
-            new(yyy, "yyy"),
-            new(yyyy, "yyyy"),
-            new(yyyyy, "yyyyy"),
+
         };
         private static readonly FormatElement[] localDateTimeElements = new FormatElement[]
         {
-            new(d, "d"),
-            new(dd, "dd"),
-            new(ddd, "ddd"),
-            new(dddd, "dddd"),
-            new(M, "M"),
-            new(MM, "MM"),
-            new(MMM, "MMM"),
-            new(MMMM, "MMMM"),
-            new(m, "m"),
-            new(mm, "mm"),
-            new(h, "h"),
-            new(hh, "hh"),
-            new(H, "H"),
-            new(HH, "HH"),
-            new(g, "g"),
-            new(f, "f"),
-            new(ff, "ff"),
-            new(fff, "fff"),
-            new(ffff, "ffff"),
-            new(fffff, "fffff"),
-            new(ffffff, "ffffff"),
-            new(fffffff, "fffffff"),
-            new(F, "F"),
-            new(FF, "FF"),
-            new(FFF, "FFF"),
-            new(FFFF, "FFFF"),
-            new(FFFFF, "FFFFF"),
-            new(FFFFFF, "FFFFFF"),
-            new(FFFFFFF, "FFFFFFF"),
-            new(s, "s"),
-            new(ss, "ss"),
-            new(t, "t"),
-            new(tt, "tt"),
-            new(t, "t"),
-            new(tt, "tt"),
-            new(y, "y"),
-            new(yy, "yy"),
-            new(yyy, "yyy"),
-            new(yyyy, "yyyy"),
-            new(yyyyy, "yyyyy"),
-            new(z, "z"),
-            new(zz, "zz"),
-            new(zzz, "zzz"),
-            new(K, "K"),
+
         };
         public static FormatStringParser TimeDeltaFormatParser { get; } = new FormatStringParser(timeDeltaElements);
         public static FormatStringParser DateTimeFormatParser { get; } = new FormatStringParser(dateTimeElements);
@@ -182,17 +60,26 @@ namespace Dyalect.Library.Core
 
             for (var i = 0; i < input.Length; i++)
             {
-                for (var j = 0; j < elements.Length; j++)
-                    if (CheckSpecifier(input, elements[i].Value, ref i))
-                    {
-                        ret.Add(elements[i]);
-                        continue;
-                    }
+                var spc = CheckSpecifiers(input, ref i);
+
+                if (spc is not null)
+                {
+                    ret.Add(spc);
+                    continue;
+                }
 
                 ret.Add(new FormatElement(Literal, input[i].ToString()));
             }
 
             return ret;
+        }
+
+        private FormatElement CheckSpecifiers(string input, ref int i)
+        {
+            for (var j = 0; j < elements.Length; j++)
+                if (CheckSpecifier(input, elements[j].Value, ref i))
+                    return elements[j];
+            return null!;
         }
 
         private bool CheckSpecifier(string input, string specifier, ref int index)
@@ -201,14 +88,15 @@ namespace Dyalect.Library.Core
 
             if (specifier.Length == 1)
                 return input[index] == specifier[0];
-            
+
             for (var i = 0; i < specifier.Length; i++)
-                if (specifier[i] != input[index++])
+                if (index >= input.Length || specifier[i] != input[index++])
                 {
                     index = preserved;
                     return false;
                 }
 
+            index--;
             return true;
         }
     }
