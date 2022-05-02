95b9561d75a5ce203728c314e7c6a00000c48b24
Василий Воронков <nic-vorov2@yandex.ru>
2022-05-02 17:54:25 +0300

    -

--- Dyalect/Runtime/ImplicitConverter.cs
+++ Dyalect/Runtime/ImplicitConverter.cs
@@ -5,25 +5,13 @@ namespace Dyalect.Runtime;
 internal static class ImplicitConverter
 {
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static long GetInt(this DyObject self)
+    public static double GetFloat(this DyObject self)
     {
+        if (self is DyFloat r8)
+            return r8.Value;
+
         if (self is DyInteger i8)
             return i8.Value;
-
-        if (self is DyFloat r8)
-            return (long)r8.Value;
-
-        throw new InvalidCastException();
-    }
-
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static string GetStr(this DyObject self)
-    {
-        if (self is DyString str)
-            return str.Value;
-
-        if (self is DyChar c)
-            return c.Value.ToString();
 
         throw new InvalidCastException();
     }
